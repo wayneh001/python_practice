c91c76ad671d77e0537f730c436162eed17eb08b
Wayne Huang <wayneh001@yahoo.com.tw>
2021-01-15 09:51:28 +0800

    Types and loop

---
+++ 003.py
@@ -0,0 +1,11 @@
+rain = input('Does it rain today?')
+
+if rain == 'Yes':
+    print('Take the unbrella')
+else: 
+    print('Enjoy the bright day')
+
+age = input('How old are you?')
+age = int(age)
+if age >= 20:
+	print('You are an adult')
