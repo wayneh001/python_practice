a53a27f206d596455099ac9ea3e8083f38e46386
Wayne Huang <wayneh001@yahoo.com.tw>
2021-01-18 10:30:39 +0800

    Final version

--- 007.number_guess.py
+++ 007.number_guess.py
@@ -1,12 +1,19 @@
 import random
-r = random.randint(1, 100)
+start = input('Set the start number. ')
+end =  input('Set the end number. ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
+count = 0
 while True:
-    num = input('Guess a number from 1 to 100. ')
+    count += 1
+    num = input('Guess a number. ')
     num = int(num)
     if num == r:
-    	print('You got it!')
+    	print(count, 'times guess. You got it!')
     	break
     elif num > r:
     	print('Try a smaller one.')
     elif num < r:
     	print('Try a bigger one.')
+    print(count, 'times guess.')
