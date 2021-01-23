b77783c94a5baa8fad4df9ca54a52c388be8096d
Wayne Huang <wayneh001@yahoo.com.tw>
2021-01-23 10:19:32 +0800

    Update of keep accounts

--- 012.keep_accounts.py
+++ 012.keep_accounts.py
@@ -1,11 +1,11 @@
 products = []
 
 while True:
-    item = input('Enter the product name.')
+    item = input('Enter the product name. ')
     if item == 'q':
     	break
-    price = input('Enter the price of the product.')
+    price = input('Enter the price of the product. ')
     products.append([item, price])
-print(products)
 
-products[0][0]
+for i in products:
+    print('The price of', i[0], 'is', i[1], '.')
