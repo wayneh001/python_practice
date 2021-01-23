fa99ebcc7369c6ad712d5f3e81ea6ba698a757d3
Wayne Huang <wayneh001@yahoo.com.tw>
2021-01-23 16:22:15 +0800

    Update keep accounts

--- 012.keep_accounts.py
+++ 012.keep_accounts.py
@@ -1,15 +1,26 @@
-products = []
+# File open
+list = []
+with open('012.my_accounts.csv', 'r', encoding = 'utf-8') as f:
+    for line in f:
+    	if 'Item, Price' in line:
+    		continue                                                                                                                                                                                                                                                                                                                                                                                    
+    	item, price = line.strip().split(',')
+    	list.append([item, price])
 
+# User key-in
 while True:
     item = input('Enter the product name. ')
     if item == 'q':
     	break
     price = input('Enter the price of the product. ')
-    products.append([item, price])
+    list.append([item, price])
 
-for i in products:
+# Key-in result print
+for i in list:
     print('The price of', i[0], 'is', i[1], '.')
     
-with open('012.my_accounts.csv', 'w') as f:
-    for i in products:
+# File save
+with open('012.my_accounts.csv', 'w', encoding = 'utf-8') as f:
+    f.write('Item, Price\n')
+    for i in list:
     	f.write(i[0] + ', ' + i[1] + '\n')
